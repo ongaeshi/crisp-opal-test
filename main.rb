7942bea54da3f5465714f3219ad9ac96e7520ad4
ongaeshi <ongaeshi0621@gmail.com>
2021-12-22 02:58:03 +0900

    feat: OPTIONSも設定可能に

--- main.rb
+++ main.rb
@@ -7,7 +7,16 @@ DESCRIPTION = <<EOS
 From RUBY
 EOS
 
+OPTIONS = {
+  "isPlayingBgm": true,
+  "isReplayEnabled": false,
+  # If you want to play a different BGM or SE,
+  # you can try changing the 'seed' value used to generate sounds.
+  "seed": 1,
+}
+
 %x{
 title = #{TITLE};
 description = #{DESCRIPTION};
+options = #{OPTIONS.to_n};
 }
