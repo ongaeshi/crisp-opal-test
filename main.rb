5a449d05f5df154ef66415866b9c5ca59dc838a5
ongaeshi <ongaeshi0621@gmail.com>
2021-12-22 03:30:20 +0900

    feat: updateもRubyで記述可能に

--- main.rb
+++ main.rb
@@ -1,22 +1,37 @@
 # Write the game name to 'title'.
-TITLE = "PIN CLIMB(RUBY)"
+TITLE = "crisp from RUBY"
 
 # 'description' is displayed on the title screen.
 DESCRIPTION = <<EOS
-[Hold] Stretch
-From RUBY
+01234656789
+abcdefghijklmnop
+ABCDEFGHIJKLMNOP
+!"#$%&'()@=~-=|{
 EOS
 
 OPTIONS = {
-  "isPlayingBgm": true,
-  "isReplayEnabled": false,
+  "isPlayingBgm": false, # TODO: リリース時はtrue
+  "isReplayEnabled": true,
   # If you want to play a different BGM or SE,
   # you can try changing the 'seed' value used to generate sounds.
   "seed": 1,
 }
 
+# TODO:
+# // User-defined characters can be written here.
+# characters = [];
+
+# 'update()' is called every frame (60 times per second).
+def update
+  1.upto(5) do |e|
+    JS.box(20 * e, 20 * e, 15, 15)
+  end
+end
+
+# TODO: 別スクリプトに逃がす
 %x{
 title = #{TITLE};
 description = #{DESCRIPTION};
 options = #{OPTIONS.to_n};
 }
+
