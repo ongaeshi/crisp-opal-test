a69ffc8dcc2f7c7cddfcefccbf8dc5c881130d0f
ongaeshi <ongaeshi0621@gmail.com>
2021-12-27 01:19:10 +0900

    refactor: Move code to arguments

--- main.rb
+++ main.rb
@@ -1,30 +1,23 @@
-# Write the game name to 'title'.
-title = "crisp-opal-test"
-
-# 'description' is displayed on the title screen.
-description = <<EOS
-01234656789
-abcdefghijklmnop
-ABCDEFGHIJKLMNOP
-!"#$%&'()@=~-=|{
-EOS
-
-options = {
-  "isPlayingBgm": false, # TODO: リリース時はtrue
-  "isReplayEnabled": true,
-  # If you want to play a different BGM or SE,
-  # you can try changing the 'seed' value used to generate sounds.
-  "seed": 1,
-}
-
-# TODO:
-# // User-defined characters can be written here.
-# characters = [];
-
 setup(
-  title: title,
-  description: description,
-  options: options
+  # Write the game name to 'title'.
+  title: "crisp-opal-test",
+  # 'description' is displayed on the title screen.
+  description: <<~EOS,
+    01234656789
+    abcdefghijklmnop
+    ABCDEFGHIJKLMNOP
+    !"#$%&'()@=~-=|{
+  EOS
+  # User-defined characters can be written here.
+  # characters: [],
+  # Configure game options.
+  options: {
+    isPlayingBgm: false, # TODO: リリース時はtrue
+    isReplayEnabled: true,
+    # If you want to play a different BGM or SE,
+    # you can try changing the 'seed' value used to generate sounds.
+    seed: 1
+  }
 )
 
 # 'update()' is called every frame (60 times per second).
