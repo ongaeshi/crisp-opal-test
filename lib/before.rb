53a94fb71d622b8e362fac1543bdc3bd55f46479
ongaeshi <ongaeshi0621@gmail.com>
2021-12-27 01:46:54 +0900

    feat: box, rect, bar, line, arc を実装

--- lib/before.rb
+++ lib/before.rb
@@ -7,10 +7,26 @@ def setup(opts = {})
   win[:options] = opts[:options].to_n if opts[:options]
 end
 
-def rect(x, y, width, height)
-  JS.call(:rect, x, y, width, height)
+def color(color_name)
+  JS.call(:color, color_name)
 end
 
 def box(x, y, width, height)
   JS.call(:box, x, y, width, height)
+end
+
+def rect(x, y, width, height)
+  JS.call(:rect, x, y, width, height)
+end
+
+def bar(x, y, length, thickness, rotate = 0.5, center_pos_ratio = 0.5)
+  JS.call(:bar, x, y, length, thickness, rotate, center_pos_ratio)
+end
+
+def line(x1, y1, x2, y2, thickness = 3)
+  JS.call(:line, x1, y1, x2, y2, thickness)
+end
+
+def arc(x, y, radius, thickness = 3, angle_from = 0, angle_to = Math::PI * 2)
+  JS.call(:arc, x, y, radius, thickness, angle_from, angle_to)
 end
--- main.rb
+++ main.rb
@@ -12,11 +12,11 @@ setup(
   # characters: [],
   # Configure game options.
   options: {
-    isPlayingBgm: false, # TODO: リリース時はtrue
+    isPlayingBgm: false, # true
     isReplayEnabled: true,
     # If you want to play a different BGM or SE,
     # you can try changing the 'seed' value used to generate sounds.
-    seed: 1
+    seed: 1,
   },
   # if "true", skip title mode.
   # debug: true
@@ -24,7 +24,33 @@ setup(
 
 # 'update()' is called every frame (60 times per second).
 def update
-  1.upto(5) do |e|
-    box(20 * e, 20 * e, 15, 15)
-  end
+  color("red")
+  # Draw the box.
+  # box(x, y, width, height?)
+  # box(pos, size)
+  box(20, 20, 15, 20)
+  # Draw the rectangle.
+  # rect(x, y, width, height?)
+  # rect(pos, size)
+  rect(70, 20, 20, 25)
+  # Draw the bar.
+  # bar(x, y, length, thickness, rotate, centerPosRatio?)
+  # bar(pos, length, thickness, rotate, centerPosRatio?)
+  bar(20, 70, 18, 5, 0.7, 0.5)
+  # Draw the line.
+  # line(x1, y1, x2, y2, thickness)
+  # line(p1, p2, thickness)
+  line(70, 70, 90, 80)
+  # Draw the arc.
+  # arc(x, y, radius, thickness?, angleFrom?, angleTo?)
+  # arc(pos, radius, thickness?, angleFrom?, angleTo?)
+  arc(30, 60, 20, 5, 0.1, 1.5)
+  # Draw the text.
+  # text(string, x, y, options?)
+  # text(string, p, options?)
+  text("a", 10, 20)
+  # Draw the user defined character.
+  # char(string, x, y, options?)
+  # char(string, p, options?)
+  char("a", 30, 40)
 end
